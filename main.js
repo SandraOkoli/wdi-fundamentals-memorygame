a18514b13ff996a3982200e7f19183433f2fac73
Sandra Okoli <sandraokoli@hotmail.co.uk>
2017-09-18 03:23:48 +0100

    finished

--- main.js
+++ main.js
@@ -25,34 +25,34 @@ var cards = [
 var cardsInPlay =[];
 
 var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again");
+     if (cardsInPlay[0] === cardsInPlay[1]) {
+     alert("You found a match!");
+  } else {
+    alert("Sorry, try again");
  };
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+ var cardId = parseInt(this.getAttribute('data-id'),10);
  console.log( "User flipped" + cards[cardId].rank);
  console.log(cards[cardId].cardImage);
  console.log(cards[cardId].suit);
  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+   if (cardsInPlay.length === 2) {
+   checkForMatch();
+ };
 };
-flipCard(0);
-flipCard(2);
 
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', 'images/back.png'); 
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
 
-checkForMatch();
-//var cardOne = cards[1];
-//cardsInPlay.push('cardOne');
-//console.log("User flipped " + cardOne");
+   }
+};
 
-//var cardTwo = cards[2];
-//cardsInPlay.push(cardTwo);
-//console.log("User flipped" + cardTwo");
- 
-//if (cardsInPlay.length === 2) {
-
-
- 
-
+createBoard();
